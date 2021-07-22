5b5bbfee6b926863f98c5d94b25b4c6560e0fb0c
sachinsingh <singh.sachin@neosoftmail.com>
2021-07-22 11:57:43 -0700

    initial commit

---
+++ Sum.java
@@ -0,0 +1,5 @@
+public class Sum{
+             public static void main(String args[]){
+                   int n1=10,n2=20,sum,
+                   sum=n1+n2;
+                   system.out.println("sum of numbers:"+sum);
